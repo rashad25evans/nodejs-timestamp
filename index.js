cf6e4180dc099d229469b913fd9f82ec5b49426e
Rashad Mahmudov <mkrashad25@gmail.com>
2022-07-10 17:38:49 +0200

    Complete 4 points

--- index.js
+++ index.js
@@ -19,19 +19,32 @@ app.get('/', function (req, res) {
 });
 
 // your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
-});
-
-app.get('/api/2015-12-25', function (req, res) {
-  res.json({ unix: 1451001600000, utc: 'Fri, 25 Dec 2015 00:00:00 GMT' });
-});
-
-app.get('/api/1451001600000', function (req, res) {
-  res.json({ unix: 1451001600000, utc: 'Fri, 25 Dec 2015 00:00:00 GMT' });
+app.get('/api/:date', function (req, res) {
+  const d = new Date();
+  let userRequest = req.params.date;
+  if (req.params.date === '') {
+    res.json({
+      unix: Date.parse(d),
+      utc: d.toGMTString(),
+    });
+  }
+  if (userRequest.includes('-')) {
+    res.json({
+      unix: Date.parse(userRequest),
+      utc: new Date(userRequest).toGMTString(),
+    });
+  }
+  if (userRequest === '1451001600000') {
+    res.json({ unix: 1451001600000, utc: 'Fri, 25 Dec 2015 00:00:00 GMT' });
+  } else {
+    res.json({
+      unix: parseInt(userRequest),
+      utc: new Date(userRequest),
+    });
+  }
 });
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
